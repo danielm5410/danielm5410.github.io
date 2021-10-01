f853c52dce643116f71ebdaae6d62c61cc7b47b7
danielm5410 <91744971+danielm5410@users.noreply.github.com>
2021-10-01 11:18:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Daniel Ma"
+let userName = "danielm5410 ";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
